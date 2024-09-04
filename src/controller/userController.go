15bfcbb1dad6c2952c99fd5a8dd640111e6ec7c9
Yohanes <yohanesdahlan@gmail.com>
2024-09-04 09:12:22 +0700

    redis sorted set

--- src/controller/userController.go
+++ src/controller/userController.go
@@ -3,7 +3,9 @@ package controller
 import (
 	"ambassador/src/database"
 	"ambassador/src/models"
+	"context"
 
+	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -24,21 +26,35 @@ func Admin(c *fiber.Ctx) error {
 }
 
 func Rankings(c *fiber.Ctx) error {
-	var users []models.User
+	// var users []models.User
 
-	database.DB.Find(&users, models.User{
-		IsAmbassador: true,
-	})
+	// database.DB.Find(&users, models.User{
+	// 	IsAmbassador: true,
+	// })
 
-	var result []interface{}
+	// var result []interface{}
 
-	for _, user := range users {
-		ambassador := models.Ambassador(user)
-		ambassador.CalcualteRevenue(database.DB)
+	// for _, user := range users {
+	// 	ambassador := models.Ambassador(user)
+	// 	ambassador.CalcualteRevenue(database.DB)
 
-		result = append(result, fiber.Map{
-			user.Name(): ambassador.Revenue,
-		})
+	// 	result = append(result, fiber.Map{
+	// 		user.Name(): ambassador.Revenue,
+	// 	})
+	// }
+	rangkings, err := database.Cache.ZRevRangeByScoreWithScores(context.Background(), "rangkings", &redis.ZRangeBy{
+		Min: "-inf",
+		Max: "+inf",
+	}).Result()
+
+	if err != nil {
+		return err
+	}
+
+	result := make(map[string]float64)
+
+	for _, rangking := range rangkings {
+		result[rangking.Member.(string)] = rangking.Score
 	}
 
 	return c.JSON(result)
