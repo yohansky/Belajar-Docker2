94d249fad0cb426df5493b265cf03d3d48ce7077
Yohanes <yohanesdahlan@gmail.com>
2024-10-07 21:19:18 +0700

    adding cors for checkout

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ func main() {
 
 	app.Use(cors.New(cors.Config{
 		AllowCredentials: true,
-		AllowOrigins:     "http://localhost:3000,http://localhost:4000",
+		AllowOrigins:     "http://localhost:3000,http://localhost:4000,http://localhost:5000",
 		AllowMethods:     "GET,POST,PUT,DELETE",
 	}))
 
